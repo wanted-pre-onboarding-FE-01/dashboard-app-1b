86eabc3c7432632165c70067c54b89f54cb9b28f
jeonhaekang <73621658+jeonhaekang@users.noreply.github.com>
2022-05-24 21:14:53 +0900

    feat : 광고 관리 페이지 추가

--- src/routes/ADmanaging/index.tsx
+++ src/routes/ADmanaging/index.tsx
@@ -1,7 +1,41 @@
 import styles from './admanaging.module.scss'
+import data from '../../data/wanted_FE_ad-list-data-set.json'
+import Card from './Card'
+import Dropdown from 'routes/ADmanaging/Dropdown'
+import { useRecoilValue } from 'recoil'
+import { adStatus } from 'state/dashBoard'
+import { useMemo } from 'react'
 
 const ADmanaging = () => {
-  return <h1 className={styles.h1}>대시보드</h1>
+  const adState = useRecoilValue(adStatus)
+
+  const filterData = useMemo(() => {
+    if (adState === 'all') {
+      return data.ads
+    }
+    return data.ads.filter((item) => item.status === adState)
+  }, [adState])
+
+  return (
+    <div className={styles.managing}>
+      <h1 className={styles.h1}>광고관리</h1>
+
+      <div className={styles.cardContainer}>
+        <div className={styles.categoryBox}>
+          <Dropdown />
+          <button className={styles.createBtn} type='button'>
+            광고 만들기
+          </button>
+        </div>
+
+        <ul className={styles.adList}>
+          {filterData.map((item) => (
+            <Card key={item.id} item={item} />
+          ))}
+        </ul>
+      </div>
+    </div>
+  )
 }
 
 export default ADmanaging
